4f3b0f1d4fb8269bfb5a0377375598b11afc5257
Serhiy Ctrax <spoltoratsk@gmail.com>
2024-05-19 08:56:50 +0300

    Connecting client side with server side

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path";
 import router from "./routes/router";
 
 const app = express();
@@ -12,10 +13,13 @@ dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../../client')));
 app.use('/', router());
 app.use((req, res, next) => {
     res.status(404).json({ error: "Route not found" });
 });
+
 
 mongoose.connect(process.env.DATABASE || '').then(()=>{
     console.log("Connected successfully");
@@ -26,3 +30,21 @@ mongoose.connection.on('error', (error: Error) => console.log(error));
 app.listen(process.env.PORT, ()=>{
     console.log("server running");
 });
+
+// your-project/
+// ├── client/
+// │   └── pages/
+// │       └── index.html
+// │       └── participanyPage.html
+// │       └── registration.html
+// │   └── styles
+// │       └──style.css
+// │   └── scripts
+// │       └──mainPage.js
+// │       └──participanyPage.js
+// │       └──retistr.js
+// ├── server/
+// │   └──src
+// │       └── index.ts
+// │   └──package.json
+// │   └──.env
--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import events from "./events";
 import participants from "./participants";
+import path from "path";
 
 const router = express.Router();
 
 export default (): express.Router => {
+    router.get('/', (req, res) =>{
+        res.sendFile(path.join(__dirname, '../../../client/pages/index.html'));
+    });
     events(router);
     participants(router);
     return router;
